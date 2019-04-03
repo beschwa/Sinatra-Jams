d2b468deb2b1e4d2f93946c51833f8a4870a2c3b
beschwa <naraiswar@gmail.com>
2019-04-03 16:39:22 -0400

    y3y

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,8 +7,42 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
-  end
+  get ("/") {
+  	@allArtists = Artist.all
+  	erb :welcome
+  }
+  get ("/artist/new") {
+  	erb :new_artist
+  }
+  
+  get ("/artists/:id") {
+  	@artist = Artist.find(params[:id])
+  	erb :artist_info
+  }
+
+
+  post ("/artist") {
+  	@artist = Artist.new(params[:artist])
+  	@artist.save
+  	redirect to "artists/#{@artist.id}"
+  }
+
+  get ("/artists/:id/edit") {
+  	@artist = Artist.find(params[:id])
+  	erb :edit_artist
+  }
+
+  patch ("/artists/:id") {
+  	@artist = Artist.find(params[:id])
+  	@artist.update(params[:artist])
+  	redirect to "artists/#{@artist.id}"
+  }
+
+  delete ("/artists/:id") {
+  	@artist = Artist.find(params[:id])
+  	@artist.destroy
+  	redirect to "/"
+  }
+
 
 end
